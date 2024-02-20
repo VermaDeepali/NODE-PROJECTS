01e37a0a679f4f8b23405bb925a6fe8169a9537e
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-02-20 11:59:42 +0530

    Added attachments and multiple emails code
    Added multiple ways to create transporter to send email.
    Added logic for sending emails to multiple Email Id at once.
    Added logic for embedded images and attachments in email.

--- NODEMAILER/index.js
+++ NODEMAILER/index.js
@@ -1,27 +1,67 @@
 const nodeMailer = require("nodemailer")
 require("dotenv").config()
-const {GMAIL_USER_EMAIL, GMAIL_APP_PASS} = process.env
+const { GMAIL_USER_EMAIL, GMAIL_APP_PASS } = process.env
 
 const html = `
-    <h1>Hello NodeMailer!</h1>
+    <h3>Hello NodeMailer!</h3>
     <p>How you doing??</p>
+    <img src="cid:${GMAIL_USER_EMAIL}" width="400" >
 `
+
+// three ways to create transporter to sent emails
+const Transporter1 = {
+    service: 'gmail',
+    auth: {
+        user: GMAIL_USER_EMAIL,
+        pass: GMAIL_APP_PASS
+    }
+}
+
+const Transporter2 = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
+    auth: {
+        user: GMAIL_USER_EMAIL,
+        pass: GMAIL_APP_PASS
+    }
+}
+
+const Transporter3 = {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: GMAIL_USER_EMAIL,
+        pass: GMAIL_APP_PASS
+    }
+}
+
+const emails = ['test1@gmail.com', 'test2@gmail.com']
+
 async function main() {
-    const transporter = nodeMailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: GMAIL_USER_EMAIL,
-            pass: GMAIL_APP_PASS
-        }
-    })
+    const transporter = nodeMailer.createTransport(Transporter3)
     const info = await transporter.sendMail({
         from: `Verma <${GMAIL_USER_EMAIL}>`,
-        to: 'test1@mail.com',
-        subject: 'Test 1',
-        html: html 
+        to: 'test1@gmail.com',
+        // to: emails, // multiple emails
+        subject: 'Emails with attachments and embedded images',
+        html: html,
+        attachments: [{
+            filename: 'img-1.jpg',
+            path: './images/img-1.jpg',
+            cid: GMAIL_USER_EMAIL // Send embedded images -- add in html content
+        },
+        {
+            filename: 'img-2.jpg',
+            path: './images/img-2.jpg' // Send images as attachements
+        }]
     })
+    console.log("Message info: ", info);
+    console.log("Message sent: ", info.messageId);
+    console.log(info.accepted);
+    console.log(info.rejected)
 
-    console.log("Message sent: ", info.messageId)
 }
 
 main().catch(e => console.log(e))
